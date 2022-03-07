3c4aa3d3556afdb8fe0b546a74183d995774840e
ayoharu <ail.comasuka.isolation@gm>
2022-03-07 23:33:37 +0900

    カメラ挙動検討中 パッケージとしてChineChineを導入

--- Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Scripts/FollowCamera.cs
+++ Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Scripts/FollowCamera.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    GameObject Player;
-    
+    GameObject  Player;
+    private Vector3 OffSet;
     // Start is called before the first frame update
     void Start()
     {
         // 追従するオブジェクト名を設定
         this.Player = GameObject.Find("SD_unitychan_humanoid");
+        OffSet = transform.position - Player.transform.position;
     }
 
     // Update is called once per frame
@@ -20,23 +21,25 @@ public class FollowCamera : MonoBehaviour
     {
         // プレイヤーに追従する
         Vector3 PlayerPos = this.Player.transform.position;
+        
 
         // *****座標*****
-        transform.position = new Vector3(PlayerPos.x,0.7f, PlayerPos.z - 5.0f);
+        //transform.position = new Vector3(PlayerPos.x,0.7f, PlayerPos.z - 4.5f);
         //transform.position = new Vector3(PlayerPos.x, PlayerPos.y + 0.7f, -4.0f);     // ジャンプ追従
+        transform.position = Vector3.Lerp(transform.position,Player.transform.position + OffSet,6.0f * Time.deltaTime);
 
         // *****回転*****
         //transform.Rotate = new Vector3(5.0f, 0.0f, 0.0f);
 
         // 画面外設定(x = 45.0fの地点に到達したらカメラの移動を停止)
         if(PlayerPos.x > 45.0f){
-            transform.position = new Vector3(45.0f, 0.7f, PlayerPos.z - 5.0f);
-            //transform.position = new Vector3(45.0f, PlayerPos.y + 0.7f, -6.0f);         // ジャンプ追従   
+            //transform.position = new Vector3(45.0f, 0.7f, PlayerPos.z - 4.5f);
+            transform.position = new Vector3(45.0f, PlayerPos.y + 0.7f, -6.0f);         // ジャンプ追従   
         }
         // 画面外設定(x = 45.0fの地点に到達したらカメラの移動を停止)
-        else if (PlayerPos.x < 5.0f){
-            transform.position = new Vector3(5.0f, 0.7f, PlayerPos.z - 5.0f);
-            //transform.position = new Vector3(5.0f, PlayerPos.y + 0.7f, -6.0f);          // ジャンプ追従
+        else if (PlayerPos.x < 15.0f){
+            //transform.position = new Vector3(15.0f, 0.7f, PlayerPos.z - 4.5f);
+            transform.position = new Vector3(5.0f, PlayerPos.y + 0.7f, -6.0f);          // ジャンプ追従
         }
     }
 }

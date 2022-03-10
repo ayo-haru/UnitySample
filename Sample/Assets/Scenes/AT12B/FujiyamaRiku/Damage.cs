b1bbcf019e9e0c9765d2c39c84c0c8f27ee71d4c
凌希 <wingrand2001@gmail.com>
2022-03-11 00:34:12 +0900

    ボスHPゲージ追加

---
+++ Sample/Assets/Scenes/AT12B/FujiyamaRiku/Damage.cs
@@ -0,0 +1,27 @@
+//==========================================================
+//      ボスへのダメージ
+//      作成日　2022/03/11
+//      作成者　藤山凌希
+//      
+//      <開発履歴>
+//      2022/03/11  ダメージ計算
+//
+//==========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

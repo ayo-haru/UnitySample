24fca3cf790f6051303e48a178ddbf6f9ce13e3f
ayoharu <ail.comasuka.isolation@gm>
2022-03-17 02:24:03 +0900

    コントローラー実装途中コミット

--- Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Input/Player_pad.cs
+++ Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Input/Player_pad.cs
@@ -7,13 +7,14 @@ using UnityEngine.InputSystem;                           // コントローラーを使う
 public class Player_pad : MonoBehaviour
 {
     //---変数宣言
-    private Controller PlayerAction;                    // 
+    private Controller PlayerAction;                    // ゲームパッドで設定した情報を扱うための変数
+    private InputAction move;                           // InputSystemの設定で作ったmoveを扱う
 
-    private InputAction move;
     private Rigidbody rb;
 
     [SerializeField] private float MaxSpeed = 5.0f;     // プレイヤーの最高速度
     private Vector3 ForceDirection = Vector2.zero;      // プレイヤーの動く方向(初期値0)
+    [SerializeField] private Camera PlayerCamara;       // カメラを扱う
     private Quaternion TargetRotation;                  // プレイヤーの回転を制御
     private bool AbleMove;                              // プレイヤーの動けるかどうか(true = 移動、false = 入力を受け付けない)
 
@@ -59,9 +60,14 @@ public class Player_pad : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        //ForceDirection += move.ReadValue<Vector2>().x; //* GetCameraRight();
+        SpeedCheck();
+
+        ForceDirection += move.ReadValue<Vector2>().x * GetCameraRight(PlayerCamara);
         rb.AddForce(ForceDirection, ForceMode.Impulse); // ForceMode.Impulse ... リジッドボディでかかる質量を一瞬で行う。
+        ForceDirection = Vector3.zero;                  // 毎回力が加わってしまうため一度ゼロにする。
     }
+
+
 
 
     //---攻撃アニメーションの切り替え
@@ -71,11 +77,23 @@ public class Player_pad : MonoBehaviour
     }
 
     //---カメラの向きを元に移動する
-    //private Vector3 GetCameraRight()
-    //{
-    //    Vector3 right = PlayerCamera.transform.right;
-    //    right.y = 0;
+    private Vector3 GetCameraRight(Camera PlayerCamara)
+    {
+        Vector3 right = PlayerCamara.transform.right;
+        right.y = 0;
 
-    //    return right.normalized;    // normalized ... ベクトルの正規化
-    //}
+        return right.normalized;                        // normalized ... ベクトルの正規化
+    }
+
+    //---速度測定メソッド
+    private void SpeedCheck()
+    {
+        Vector3 PlayerVelocity = rb.velocity;           // 現在の速度の値
+        PlayerVelocity.y = 0;                           // Y軸の速度を移動速度に加味しない。
+
+        if (PlayerVelocity.sqrMagnitude > MaxSpeed * MaxSpeed)  // sqrMagnitude ... ベクトルの長さの二乗を返す物
+        {
+            rb.velocity = PlayerVelocity.normalized * MaxSpeed; // 方向は叔父ベクトルだが、速度はMaxSpeedまでしかいかないようにする
+        }
+    }
 }

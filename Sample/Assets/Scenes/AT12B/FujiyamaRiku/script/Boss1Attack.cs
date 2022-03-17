586e7cf6aee8af67b6f82cd2cbae14e0ea4bc299
凌希 <wingrand2001@gmail.com>
2022-03-18 02:46:10 +0900

    処理ひどいけどイチゴ爆弾作った

--- Sample/Assets/Scenes/AT12B/FujiyamaRiku/script/Boss1Attack.cs
+++ Sample/Assets/Scenes/AT12B/FujiyamaRiku/script/Boss1Attack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Boss1Attack : MonoBehaviour
 {
     //ボスの攻撃の種類
@@ -12,10 +13,26 @@ public class Boss1Attack : MonoBehaviour
         Attack3,
         Attack4, 
     }
-    GameObject obj;
-    public GameObject [] Strawberry;
-    bool [] StrawberryFlg;
-    [SerializeField] public int StrawberryNum;
+    //イチゴ爆弾変数
+    //----------------------------------------------------------
+    GameObject obj;                             //イチゴ生成用
+    public GameObject [] Strawberry;            //イチゴ生成後格納
+    bool [] StrawberryFlg;                      //いらんきがするね
+    [SerializeField] public int Max_Strawberry;  //打ったイチゴの判断
+    private int StrawberryNum;
+    private Vector3 StrawberryPos;
+
+    //ベジエ曲線用
+    private Vector3  StartPoint;
+    private Vector3 [] MiddlePoint;
+    private Vector3 [] EndPoint;
+    [SerializeField] private Vector3 FirstMiddlePoint;
+    [SerializeField] private Vector3 FirstEndPoint;
+    private float [] FinishTime;
+    private bool SpanFlg = false;
+    //----------------------------------------------------------
+
+
 
     private BossAttack Boss1AttackState = BossAttack.Attack1;
     // Start is called before the first frame update
@@ -23,17 +40,30 @@ public class Boss1Attack : MonoBehaviour
     {
         obj = (GameObject)Resources.Load("strawberry");
         //Strawberry = new GameObject[StrawberryNum];
-        StrawberryFlg = new bool[StrawberryNum];
-        StrawberryFlg[0] = false;
+        StrawberryNum = 0;
+        Strawberry = new GameObject[Max_Strawberry];
+        StrawberryFlg = new bool[Max_Strawberry];
+        MiddlePoint = new Vector3[Max_Strawberry];
+        EndPoint = new Vector3[Max_Strawberry];
+        FinishTime = new float[Max_Strawberry];
+
+        StartPoint.x = Boss.BossPos.x;
+        StartPoint.y = Boss.BossPos.y += 2;
+        StartPoint.z = Boss.BossPos.z;
+        for (int i=0;i < Max_Strawberry;i++)
+        {
+            StrawberryFlg[i] = false;
+            MiddlePoint[i] = FirstMiddlePoint;
+            EndPoint[i] = FirstEndPoint;
+            MiddlePoint[i].x += (1.7f * i);
+            EndPoint[i].x += (4f * i);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            
-        }
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Boss1AttackState = BossAttack.Attack2;
@@ -67,16 +97,32 @@ public class Boss1Attack : MonoBehaviour
     }
     private void Boss1Attack2()
     {
-        //イチゴ
-        if (!StrawberryFlg[0])
+        for (int i = 0; i < Max_Strawberry; i++)
         {
-            obj.transform.position = new Vector3(10.0f, 2.0f, -2f);
-            Strawberry[0] = Instantiate(obj);
-            Rigidbody rb = Strawberry[0].GetComponent<Rigidbody>();  
-            Vector3 force = new Vector3(-40.0f, 40.0f, 0.0f);    // 力を設定
-            //GameObject.Find("strawberry(Clone)").GetComponent<Rigidbody>().AddForce(force);
-            rb.AddForce(force);  // 力を加える
-            StrawberryFlg[0] = true;
+            //イチゴ
+            if (!StrawberryFlg[0])
+            {
+                Strawberry[0] = Instantiate(obj, StartPoint, Quaternion.identity);
+                Debug.Log("Flg : " + StrawberryFlg[i]);
+                StrawberryFlg[0] = true;
+            }
+            if (!StrawberryFlg[i] && FinishTime[i - 1] > 0.5f)
+            {
+                Strawberry[i] = Instantiate(obj, StartPoint, Quaternion.identity);
+                Debug.Log("Flg : " + StrawberryFlg[i]);
+                StrawberryFlg[i] = true;
+            }
+
+            if (StrawberryFlg[i])
+            {
+                //イチゴ消えたら初期化するんだぞby私
+                FinishTime[i] += Time.deltaTime;
+            }
+            Vector3 S = Vector3.Lerp(StartPoint, MiddlePoint[i], FinishTime[i]);
+            Vector3 E = Vector3.Lerp(MiddlePoint[i], EndPoint[i], FinishTime[i]);
+            Strawberry[i].transform.position = Vector3.Lerp(S, E, FinishTime[i]);
+
+            
         }
     }
     private void Boss1Attack3()
@@ -86,5 +132,11 @@ public class Boss1Attack : MonoBehaviour
     private void Boss1Attack4()
     {
         //スーパー着地
+    }
+
+    //ベジエ曲線
+    private void GetPoint()
+    {
+        
     }
 }

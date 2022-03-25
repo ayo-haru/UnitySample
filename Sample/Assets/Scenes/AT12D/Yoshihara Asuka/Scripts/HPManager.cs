21e69c34e4733240571820d5fc9a26db88a37bc8
ayoharu <ail.comasuka.isolation@gm>
2022-03-26 04:08:55 +0900

    バーのHPをとりあえず実装

--- Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Scripts/HPManager.cs
+++ Sample/Assets/Scenes/AT12D/Yoshihara Asuka/Scripts/HPManager.cs
@@ -11,22 +11,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPManager : MonoBehaviour
 {
+    public Slider ゲージA;
+
     private void Awake()
     {
         //---開始時に表示するUI(GameObject)のアクティブ状態を設定 true = 有効 / false = 無効
-        GameObject.Find("Full Moon").SetActive(true);
-        GameObject.Find("Harf Moon1").SetActive(false);
-        GameObject.Find("Harf Moon2").SetActive(false);
-        GameObject.Find("Harf Moon3").SetActive(false);
-        GameObject.Find("New Moon").SetActive(false);
+        //GameObject.Find("Full Moon").SetActive(true);
+        //GameObject.Find("Harf Moon1").SetActive(false);
+        //GameObject.Find("Harf Moon2").SetActive(false);
+        //GameObject.Find("Harf Moon3").SetActive(false);
+        //GameObject.Find("New Moon").SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        ゲージA.value = 5;
     }
 
     // Update is called once per frame

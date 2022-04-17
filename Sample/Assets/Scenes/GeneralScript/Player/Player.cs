625b63c702f578b622b05e0e0152ece2d3ab6e5d
ayoharu <ail.comasuka.isolation@gm>
2022-04-17 23:58:36 +0900

    途中コミット

--- Sample/Assets/Scenes/GeneralScript/Player/Player.cs
+++ Sample/Assets/Scenes/GeneralScript/Player/Player.cs
@@ -30,7 +30,7 @@ public class Player : MonoBehaviour
     {
         GameData.PlayerPos = this.transform.position;    // プレイヤーの位置を保存
 
-        if (this.transform.position.y < -5)
+        if (this.transform.position.y < -10000)
         {
             this.transform.position = GameData.Player.transform.position = GameData.PlayerPos = ReSpawnPos;
         }

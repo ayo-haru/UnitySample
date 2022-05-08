39d21e346b2621d516c0dd0b8125f1ac227f9a82
OgusuYuuko <yuuko_ogusu@outlook.jp>
2022-05-08 15:58:57 +0900

    オプションに戻るを追加

--- Sample/Assets/Scenes/GeneralScript/SceneManager/TitleSceneManager.cs
+++ Sample/Assets/Scenes/GeneralScript/SceneManager/TitleSceneManager.cs
@@ -223,14 +223,8 @@ public class TitleSceneManager : MonoBehaviour {
             {
                 // 決定音
                 SoundManager.Play(SoundData.eSE.SE_KETTEI, SoundData.TitleAudioList);
-                //オプションマネージャーのアクティブを反転
-                Optionmanager.SetActive(!Optionmanager.activeSelf);
-                //オプション閉じたときに音量保存
-                if (!Optionmanager.activeSelf)
-                {
-                    SaveManager.saveSEVolume(SoundManager.seVolume);
-                    SaveManager.saveBGMVolume(SoundManager.bgmVolume);
-                }
+                //オプションマネージャーをアクティブにする
+                Optionmanager.SetActive(true);
                 isDecision = false;
             }
 
@@ -328,7 +322,7 @@ public class TitleSceneManager : MonoBehaviour {
     /// </summary>
     private void OnDecision(InputAction.CallbackContext obj)
     {
-        if (!isPressButton /*|| Optionmanager.activeSelf*/)
+        if (!isPressButton || Optionmanager.activeSelf)
         {
             return;
         }

62b981b2fb456ecf657be442481082bbf4608b43
凌希 <wingrand2001@gmail.com>
2022-05-14 11:23:38 +0900

    ボスの死ぬとき演出一応完成

--- Sample/Assets/Scenes/AT12B/FujiyamaRiku/script/Boss1Manager.cs
+++ Sample/Assets/Scenes/AT12B/FujiyamaRiku/script/Boss1Manager.cs
@@ -87,7 +87,7 @@ public class Boss1Manager : MonoBehaviour
                         EffectManager.Play(EffectData.eEFFECT.EF_BOSSKILL,new Vector3(Boss.transform.position.x, Boss.transform.position.y, Boss.transform.position.z),8f);
                         PlayEffect = true;
                     }
-
+                    GetComponent<BossDeathCam>().DeathCamera();
                      Destroy(Boss,7.9f);
 
                     //Warp.SetActive(true);

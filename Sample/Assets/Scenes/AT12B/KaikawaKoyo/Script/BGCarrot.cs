728a309a4d17a8911f2424859b103723c0bffc09
Kaikawa13 <seariver-mt4bwy@i.softbank.jp>
2022-05-26 23:09:21 +0900

    ステージ1わちゃわちゃしました

---
+++ Sample/Assets/Scenes/AT12B/KaikawaKoyo/Script/BGCarrot.cs
@@ -0,0 +1,36 @@
+//==========================================================
+//      ”wŒi‚Ìƒjƒ“ƒWƒ“
+//      ì¬“ú@2022/05/26
+//      ì¬Ò@ŠCìW—k
+//      
+//      <ŠJ”­—š—ğ>
+//      2022/05/26      
+//
+//==========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGCarrot : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 rotpos;
+
+    [SerializeField]
+    private float speed;
+
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position + rotpos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(position, Vector3.down, speed);
+    }
+
+}
